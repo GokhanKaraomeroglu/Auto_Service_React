01d269a41e6ba31d56a675467617dec7166c7a03
Himmel König <itogreniyorum@gmail.com>
2022-08-31 00:22:45 +0300

    Router changed

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -12,6 +12,18 @@ import Footer from "../components/Footer";
 import Periodic from "../pages/hizmetler/Periodic";
 import Maintenance from "../pages/hizmetler/Maintenance";
 import Accident from "../pages/hizmetler/Accident";
+import Insurance from "../pages/hizmetler/Insurance";
+
+import Motor from "../pages/servis/Motor";
+import Boya from "../pages/servis/Boya";
+import Mekanik from "../pages/servis/Mekanik";
+import Kaborta from "../pages/servis/Kaborta";
+import Elektrik from "../pages/servis/Elektrik";
+
+import Unalusta from "../pages/about/Unalusta";
+import About from "../pages/about/About";
+import Admin from "../pages/about/Admin";
+import Teknik from "../pages/about/Teknik";
 
 
 // import {Login, Register } from "../pages/LoginRegister";
@@ -28,19 +40,26 @@ function AppRouter() {
     <BrowserRouter>
 		<NavbarTail/>
       <Routes>
-        {/* <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register/>} /> */}
-
+        
         <Route path="/" element={<Homepage /> } />
+
         <Route path="/periodic" element={<Periodic /> } />
         <Route path="/maintenance" element={<Maintenance /> } />
         <Route path="/accident" element={<Accident /> } />
-        <Route path="/goatPage" element={<GoatPage /> } />
-        <Route path="/sheepPage" element={<SheepPage /> } />
-        <Route path="/horsePage" element={<HorsePage /> } />
-        <Route path="/poultryPage" element={<PoultryPage /> } />
-        <Route path="about" element={<About />} /> */}
-        {/* <Route path="/auth" element={<SignUp />} />
+        <Route path="/insurance" element={<Insurance /> } />
+
+        <Route path="/motor" element={<Motor /> } />
+        <Route path="/boya" element={<Boya /> } />
+        <Route path="/elektrik" element={<Elektrik /> } />
+        <Route path="/kaborta" element={<Kaborta /> } />
+        <Route path="/mekanik" element={<Mekanik /> } />
+        
+        <Route path="/unalusta" element={<Unalusta /> } />
+        <Route path="/about" element={<About /> } />
+        <Route path="/teknik" element={<Teknik /> } />
+        <Route path="/admin" element={<Admin /> } />
+        
+      
       </Routes>
 			<Footer/>
       </BrowserRouter>
