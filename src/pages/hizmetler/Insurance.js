62711a7760bfbe0441c81faa9f1c6be0ee15b4bf
Himmel König <itogreniyorum@gmail.com>
2022-08-30 23:59:46 +0300

    Hizmetler Pages added

---
+++ src/pages/hizmetler/Insurance.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Insurance() {
+	return (
+		<div>Kaza Sonrası</div>
+	)
+}
+
+export default Insurance
---
+++ src/pages/hizmetler/Periodic.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Periodic() {
+	return (
+		<div>Periyodik</div>
+	)
+}
+
+export default Periodic
