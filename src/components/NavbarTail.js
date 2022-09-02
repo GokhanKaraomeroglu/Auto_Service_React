58a2fb49ce9e9329c200d9b6f84fa89d164b7952
Himmel König <itogreniyorum@gmail.com>
2022-09-02 15:00:07 +0300

    Navbar changed

--- src/components/NavbarTail.js
+++ src/components/NavbarTail.js
@@ -19,29 +19,53 @@ import {
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import Logo from '../assets/Logo.png'
 
+const aboutUs = [
+  {
+    name: 'Ünal Usta',
+    description: "Önemli Oto Servisi'nin kurucusu",
+    href: '/unalusta',
+    icon: SparklesIcon,
+  },
+  {
+    name: 'Yönetim',
+    description: 'Tecrübeli yönetim kadromuz.',
+    href: '/admin',
+    icon: Cog8ToothIcon,
+  },
+  { name: 'Teknik Ekip', 
+  description: "Alanında uzaman teknik ekibimiz ile hizmetinizdeyiz.", 
+  href: '/teknik', 
+  icon: WrenchIcon },
+  {
+    name: 'Hakkımızda',
+    description: "Tecrübeli yönetim kadromuz ve teknik ekibimizle hizmetinizdeyiz.",
+    href: '/about',
+    icon: BookmarkSquareIcon,
+  },
+]
 const solutions = [
   {
     name: 'Motor',
-    description: 'Araçlarınızın onarımında yüksek kaliteli yedek parçalar ve en hassas ellerden çıkan işcilik kullanılmaktaır.',
+    description: 'Sağlıklı bir motor aracınızı güvenli ve tasarruflu yapar.',
     href: '/motor',
     icon: Cog8ToothIcon,
   },
   {
     name: 'Mekanik Aksam',
-    description: 'Araçlarınızın onarımında yüksek kaliteli yedek parçalar ve en hassas ellerden çıkan işcilik kullanılmaktaır.',
+    description: 'Aracınızın tekrar arızalanmaması için bize güvenin.',
     href: '/mekanik',
     icon: WrenchIcon,
   },
-  { name: 'Kaborta', description: "Araçlarınızın onarımında yüksek kaliteli yedek parçalar ve en hassas ellerden çıkan işcilik kullanılmaktaır.", href: '/kaborta', icon: ShieldCheckIcon },
+  { name: 'Kaborta', description: "Uzman kabortacılarımızın elinde aracınız güvende.", href: '/kaborta', icon: ShieldCheckIcon },
   {
     name: 'Boya',
-    description: "Araçlarınızın onarımında yüksek kaliteli yedek parçalar ve en hassas ellerden çıkan işcilik kullanılmaktaır.",
+    description: "Aracınızın güzel görünmesindeki en önemli faktör.",
     href: '/boya',
     icon: SparklesIcon,
   },
   {
     name: 'Oto Elektrik',
-    description: 'Araçlarınızın onarımında yüksek kaliteli yedek parçalar ve en hassas ellerden çıkan işcilik kullanılmaktaır.',
+    description: 'Araçlarınızın elektrik donanımlarına hassas dokunuşlar.',
     href: '/elektrik',
     icon: ArrowPathIcon,
   },
@@ -59,7 +83,7 @@ const resources = [
   },
   {
     name: 'Onarım',
-    description: 'Onarım yapmak için derin tecrübemizi ve yeteneğimizi kullanıyoruz.',
+    description: 'Araçlarınızın onarımında yüksek kaliteli yedek parçalar ve en hassas ellerden çıkan işcilik kullanılmaktaır.',
     href: '/maintenance',
     icon: BookmarkSquareIcon,
   },
@@ -103,6 +127,71 @@ export default function NavbarTail() {
             </Popover.Button>
           </div>
           <Popover.Group as="nav" className="hidden md:flex space-x-10">
+            <Popover className="relative">
+              {({ open }) => (
+                <>
+                  <Popover.Button
+                    className={classNames(
+                      open ? 'text-gray-900' : 'text-gray-500',
+                      'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                    )}
+                  >
+                    <span>Hakkımızda</span>
+                    <ChevronDownIcon
+                      className={classNames(
+                        open ? 'text-gray-600' : 'text-gray-400',
+                        'ml-2 h-5 w-5 group-hover:text-gray-500'
+                      )}
+                      aria-hidden="true"
+                    />
+                  </Popover.Button>
+
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-200"
+                    enterFrom="opacity-0 translate-y-1"
+                    enterTo="opacity-100 translate-y-0"
+                    leave="transition ease-in duration-150"
+                    leaveFrom="opacity-100 translate-y-0"
+                    leaveTo="opacity-0 translate-y-1"
+                  >
+                    <Popover.Panel className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
+                      <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                        <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                          {aboutUs.map((item) => (
+                            <a
+                              key={item.name}
+                              href={item.href}
+                              className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
+                            >
+                              <item.icon className="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
+                              <div className="ml-4">
+                                <p className="text-base font-medium text-gray-900">{item.name}</p>
+                                <p className="mt-1 text-sm text-gray-500">{item.description}</p>
+                              </div>
+                            </a>
+                          ))}
+                        </div>
+                        {/* <div className="px-5 py-5 bg-gray-50 space-y-6 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
+                          {callsToAction.map((item) => (
+                            <div key={item.name} className="flow-root">
+                              <a
+                                href={item.href}
+                                className="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-100"
+                              >
+                                <item.icon className="flex-shrink-0 h-6 w-6 text-gray-400" aria-hidden="true" />
+                                <span className="ml-3">{item.name}</span>
+                              </a>
+                            </div>
+                          ))}
+                        </div> */}
+                      </div>
+                    </Popover.Panel>
+                  </Transition>
+                </>
+              )}
+            </Popover>
+
             <Popover className="relative">
               {({ open }) => (
                 <>
