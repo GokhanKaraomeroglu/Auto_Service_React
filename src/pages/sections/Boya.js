5361b1327d1162900e777d234aa1bca505257fb8
Himmel König <itogreniyorum@gmail.com>
2022-09-09 10:22:27 +0300

    Boya Piics added

--- src/pages/sections/Boya.js
+++ src/pages/sections/Boya.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import MotorPic from "../../assets/Motor.jpg"
-import MotorPic2 from "../../assets/Motor2.jpg"
+import BoyaPic from "../../assets/Boya.jpg"
+import BoyaPic2 from "../../assets/Boya2.jpeg"
 
 function Boya() {
 	return (
@@ -27,7 +27,7 @@ function Boya() {
             <img
               className="object-cover object-center rounded"
               alt="Motor"
-              src={MotorPic}
+              src={BoyaPic}
             />
           </div>
         </div>
@@ -35,7 +35,7 @@ function Boya() {
 			<section class="text-gray-600 body-font"> */}
   <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
     <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-      <img class="object-cover object-center rounded" alt="hero" src={MotorPic2}/>
+      <img class="object-cover object-center rounded" alt="hero" src={BoyaPic2}/>
     </div>
     <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
       <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Motor Bakımı {' '}
